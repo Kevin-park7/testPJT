05795a4e918f75a4180581633269a0bb7c8436d6
성원 박 <tjddnjs8503@naver.com>
2021-12-15 15:28:54 +0900

    Create

---
+++ z.testPJT/src/main/java/domain/product.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class product {
+
+	public product() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ z.testPJT/src/main/java/product/productImp.java
@@ -0,0 +1,9 @@
+package product;
+
+public class productImp {
+
+	public productImp() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
